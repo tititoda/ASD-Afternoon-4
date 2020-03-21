f59e77c5ba0a57c2a5340dac1c3a093424901c2a
thof97 <thomas.hofstaetter@student.tugraz.at>
2020-03-21 13:58:20 +0100

    navigation to addRecipe and function to add imgs

--- COOK/app/src/main/java/com/example/cook/add_recipe.java
+++ COOK/app/src/main/java/com/example/cook/add_recipe.java
@@ -2,13 +2,52 @@ package com.example.cook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+
+import java.io.FileNotFoundException;
 
 public class add_recipe extends AppCompatActivity {
+
+    ImageView recipe_image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_recipe);
+        Button add_picture_button = findViewById(R.id.add_picture);
+        recipe_image = findViewById(R.id.imageView);
+
+        add_picture_button.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View arg0) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, 0);
+            }});
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK) {
+            Uri image_uri = data.getData();
+            Bitmap bitmap;
+            try {
+                bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(image_uri));
+                recipe_image.setImageBitmap(bitmap);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
