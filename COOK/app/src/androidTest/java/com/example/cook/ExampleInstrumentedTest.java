2e32e48e588e5cc44f0168f52dda39a2f817f22c
Kina Churtova <churtova@student.tugraz.at>
2020-04-29 13:35:42 +0200

    COOK-02 Add recipe, Instrumented test  [CK1, JD2]

--- COOK/app/src/androidTest/java/com/example/cook/ExampleInstrumentedTest.java
+++ COOK/app/src/androidTest/java/com/example/cook/ExampleInstrumentedTest.java
@@ -1,22 +1,46 @@
 package com.example.cook;
 
 import android.content.Context;
+import android.content.Intent;
+import android.widget.EditText;
 
-import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.espresso.action.ScrollToAction;
+import androidx.test.rule.ActivityTestRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+import androidx.test.platform.app.InstrumentationRegistry;
 
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
+import static androidx.test.espresso.action.ViewActions.swipeDown;
+import static androidx.test.espresso.action.ViewActions.swipeUp;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withInputType;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+
+
 @RunWith(AndroidJUnit4.class)
+@LargeTest
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivityRule
+            = new ActivityTestRule<>(MainActivity.class, true, true);
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
@@ -24,4 +48,24 @@ public class ExampleInstrumentedTest {
 
         assertEquals("com.example.cook", appContext.getPackageName());
     }
+
+    @Test
+    public void addRecipe() throws InterruptedException {
+        onView(withId(R.id.floatingActionButtonAddRecipe)).perform(click());
+
+        onView(withId(R.id.inputName)).perform(typeText("test name"));
+        onView(withId(R.id.inputDescription)).perform(typeText("test description"));
+        onView(withId(R.id.inputPreparationTime)).perform(typeText("13"));
+        onView(withId(R.id.inputCookingTime)).perform(typeText("13"));
+
+        onView(withId(R.id.inputSbs)).perform(scrollTo());
+        onView(withId(R.id.inputSbs)).perform(typeText("test sbs"));
+
+        onView(withId(R.id.tag0)).perform(scrollTo());
+        onView(withId(R.id.tag0)).perform(click());
+
+        onView(withId(R.id.submitRecipe)).perform(scrollTo());
+        onView(withId(R.id.submitRecipe)).perform(click());
+    }
+
 }
--- COOK/app/src/main/java/com/example/cook/AddRecipe.java
+++ COOK/app/src/main/java/com/example/cook/AddRecipe.java
@@ -1,8 +1,11 @@
 package com.example.cook;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -72,8 +75,7 @@ public class AddRecipe extends AppCompatActivity {
         tag8 = (CheckBox) findViewById(R.id.tag8);
         tag9 = (CheckBox) findViewById(R.id.tag9);
 
-        if(Recipe.edit_recipe == true)
-        {
+        if (Recipe.edit_recipe == true) {
             input_name.setText(Recipe.recipe_to_edit.getName());
             input_description.setText(Recipe.recipe_to_edit.getDescription());
             input_prep_time.setText(String.valueOf(Recipe.recipe_to_edit.getPrep_time()));
@@ -101,10 +103,8 @@ public class AddRecipe extends AppCompatActivity {
             tags[7] = Recipe.recipe_to_edit.isLunch();
             tags[8] = Recipe.recipe_to_edit.isFast_food();
             tags[9] = Recipe.recipe_to_edit.isSoup();
-        }
-        else
-        {
-            for(int i = 0; i < 10; i++) {
+        } else {
+            for (int i = 0; i < 10; i++) {
                 tags[i] = false;
             }
         }
@@ -112,111 +112,128 @@ public class AddRecipe extends AppCompatActivity {
         tag0.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(tag0.isChecked()){
+                if (tag0.isChecked()) {
                     tags[0] = true;
-                }
-                else tags[0] = false;
+                } else tags[0] = false;
             }
         });
         tag1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(tag1.isChecked()){
+                if (tag1.isChecked()) {
                     tags[1] = true;
-                }
-                else tags[1] = false;
+                } else tags[1] = false;
             }
         });
         tag2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(tag2.isChecked()){
+                if (tag2.isChecked()) {
                     tags[2] = true;
-                }
-                else tags[2] = false;
+                } else tags[2] = false;
             }
         });
         tag3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(tag3.isChecked()){
+                if (tag3.isChecked()) {
                     tags[3] = true;
-                }
-                else tags[3] = false;
+                } else tags[3] = false;
             }
         });
         tag4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(tag4.isChecked()){
+                if (tag4.isChecked()) {
                     tags[4] = true;
-                }
-                else tags[4] = false;
+                } else tags[4] = false;
             }
         });
         tag5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(tag5.isChecked()){
+                if (tag5.isChecked()) {
                     tags[5] = true;
-                }
-                else tags[5] = false;
+                } else tags[5] = false;
             }
         });
         tag6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(tag6.isChecked()){
+                if (tag6.isChecked()) {
                     tags[6] = true;
-                }
-                else tags[6] = false;
+                } else tags[6] = false;
             }
         });
         tag7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(tag7.isChecked()){
+                if (tag7.isChecked()) {
                     tags[7] = true;
-                }
-                else tags[7] = false;
+                } else tags[7] = false;
             }
         });
         tag8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(tag8.isChecked()){
+                if (tag8.isChecked()) {
                     tags[8] = true;
-                }
-                else tags[8] = false;
+                } else tags[8] = false;
             }
         });
         tag9.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(tag9.isChecked()){
+                if (tag9.isChecked()) {
                     tags[9] = true;
-                }
-                else tags[9] = false;
+                } else tags[9] = false;
             }
         });
 
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                name = input_name.getText().toString();
-                assert(!name.isEmpty());
+                if(input_name.getText().toString().isEmpty()) showError("recipe name");
+                else if(input_description.getText().toString().isEmpty()) showError("recipe description");
+                else if(input_prep_time.getText().toString().isEmpty()) showError("preparation time");
+                else if(input_cooking_time.getText().toString().isEmpty()) showError("cooking time");
+                else if(input_sbs_description.getText().toString().isEmpty()) showError("step by step description");
+                else if(!checkTags()) showError("any tags");
+                else
+                {
+                    name = input_name.getText().toString();
+                    description = input_description.getText().toString();
+                    prep_time = Integer.parseInt(input_prep_time.getText().toString());
+                    cooking_time = Integer.parseInt(input_cooking_time.getText().toString());
+                    sbs_description = input_sbs_description.getText().toString();
 
-                description = input_description.getText().toString();
-                assert(!description.isEmpty());
+                    if (Recipe.edit_recipe == true) {
+                        Recipe.recipe_to_edit.setName(name);
+                        Recipe.recipe_to_edit.setDescription(description);
+                        Recipe.recipe_to_edit.setPrep_time(prep_time);
+                        Recipe.recipe_to_edit.setCooking_time(cooking_time);
+                        Recipe.recipe_to_edit.setSBSDescription(sbs_description);
 
-                prep_time = Integer.parseInt(input_prep_time.getText().toString());
-                assert(prep_time != 0);
+                        Recipe.recipe_to_edit.setPasta(tags[0]);
+                        Recipe.recipe_to_edit.setMeat(tags[1]);
+                        Recipe.recipe_to_edit.setDinner(tags[2]);
+                        Recipe.recipe_to_edit.setBreakfast(tags[3]);
+                        Recipe.recipe_to_edit.setSweets(tags[4]);
+                        Recipe.recipe_to_edit.setHealthy(tags[5]);
+                        Recipe.recipe_to_edit.setVegan(tags[6]);
+                        Recipe.recipe_to_edit.setLunch(tags[7]);
+                        Recipe.recipe_to_edit.setFast_food(tags[8]);
+                        Recipe.recipe_to_edit.setSoup(tags[9]);
 
-                cooking_time = Integer.parseInt(input_cooking_time.getText().toString());
-                assert(cooking_time != 0);
+                    } else {
+                        Recipe.new_recipe_added = true;
+                    }
 
-                sbs_description = input_sbs_description.getText().toString();
-                assert(!sbs_description.isEmpty());
+                    Intent go_back_to_main = new Intent(AddRecipe.this, MainActivity.class);
+                    startActivity(go_back_to_main);
+
+                    finish();
+                }
 
                 //MainActivity.recipes.add(new Recipe(100, name, description, prep_time,
                 //       cooking_time, food_picture, sbs_description, getTags()));
@@ -244,35 +261,6 @@ public class AddRecipe extends AppCompatActivity {
 
                 //Test
                 //description.setText(name);
-
-                if(Recipe.edit_recipe == true)
-                {
-                    Recipe.recipe_to_edit.setName(name);
-                    Recipe.recipe_to_edit.setDescription(description);
-                    Recipe.recipe_to_edit.setPrep_time(prep_time);
-                    Recipe.recipe_to_edit.setCooking_time(cooking_time);
-                    Recipe.recipe_to_edit.setSBSDescription(sbs_description);
-
-                    Recipe.recipe_to_edit.setPasta(tags[0]);
-                    Recipe.recipe_to_edit.setMeat(tags[1]);
-                    Recipe.recipe_to_edit.setDinner(tags[2]);
-                    Recipe.recipe_to_edit.setBreakfast(tags[3]);
-                    Recipe.recipe_to_edit.setSweets(tags[4]);
-                    Recipe.recipe_to_edit.setHealthy(tags[5]);
-                    Recipe.recipe_to_edit.setVegan(tags[6]);
-                    Recipe.recipe_to_edit.setLunch(tags[7]);
-                    Recipe.recipe_to_edit.setFast_food(tags[8]);
-                    Recipe.recipe_to_edit.setSoup(tags[9]);
-
-                }
-                else {
-                    Recipe.new_recipe_added = true;
-                }
-
-                Intent go_back_to_main = new Intent(AddRecipe.this, MainActivity.class);
-                startActivity(go_back_to_main);
-
-                finish();
             }
         });
 
@@ -298,12 +286,13 @@ public class AddRecipe extends AppCompatActivity {
 
         recipe_image = findViewById(R.id.imageView);
 
-        add_picture_button.setOnClickListener(new Button.OnClickListener(){
+        add_picture_button.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View arg0) {
                 Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                 startActivityForResult(intent, 0);
-            }});
+            }
+        });
 
 
     }
@@ -312,7 +301,7 @@ public class AddRecipe extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(resultCode == RESULT_OK) {
+        if (resultCode == RESULT_OK) {
             Uri image_uri = data.getData();
             Bitmap bitmap;
             try {
@@ -336,4 +325,29 @@ public class AddRecipe extends AppCompatActivity {
 
         });
     }*/
+
+    private boolean checkTags()
+    {
+        for(int i = 0; i < 10; i++)
+        {
+            if(tags[i] == true) return true;
+        }
+
+        return false;
+    }
+
+    private void showError(String attribute)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(AddRecipe.this);
+        builder.setTitle("You didn't enter " + attribute + ".");
+        builder.setMessage("Please try again.");
+
+        builder.setNegativeButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.show();
+    }
 }
